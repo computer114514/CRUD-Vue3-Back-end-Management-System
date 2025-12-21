a5882c95fa4a972082cea58ceb57527507cf9b07
computer114514 <3058094441@qq.com>
2025-12-21 20:55:36 +0800

    start dashBoard

--- src/views/DashBoard.vue
+++ src/views/DashBoard.vue
@@ -1,10 +1,38 @@
 <template>
   <div>
     <el-row :gutter="20">
-      <el-col :span="6"><el-card></el-card></el-col>
-      <el-col :span="6"><el-card></el-card></el-col>
-      <el-col :span="6"><el-card></el-card></el-col>
-      <el-col :span="6"><el-card></el-card></el-col>
+      <el-col :span="6"><el-card body-class="card-body">
+        <el-icon class="card-icon bg1">
+          <User></User>
+        </el-icon>
+        <div class="card-content">
+          <div>用户访问量</div>
+        </div>
+      </el-card></el-col>
+      <el-col :span="6"><el-card body-class="card-body">
+        <el-icon class="card-icon bg2">
+          <ChatDotRound />
+        </el-icon>
+        <div class="card-content">
+          <div>系统信息</div>
+        </div>
+      </el-card></el-col>
+      <el-col :span="6"><el-card body-class="card-body">
+        <el-icon class="card-icon bg3">
+          <Goods />
+        </el-icon>
+        <div class="card-content">
+          <div>商品数量</div>
+        </div>
+      </el-card></el-col>
+      <el-col :span="6"><el-card body-class="card-body">
+        <el-icon class="card-icon bg4">
+          <ShoppingCartFull />
+        </el-icon>
+        <div class="card-content">
+          <div>今日订单</div>
+        </div>
+      </el-card></el-col>
     </el-row>
     <el-row :gutter="20">
       <el-col :span="18"><el-card></el-card></el-col>
@@ -24,15 +52,50 @@
 
 <style scoped>
 
-  /* .el-col{
-    background-color: skyblue;
-  } */
-   .el-row {
+.el-col{
+  min-width: 0;
+  }
+.el-row {
   margin-bottom: 20px;
+  min-width: 0;
 }
 .el-card{
   overflow: hidden;
   border-radius: 5px;
   /* border-radius:5%l是不同的！我说怎么这么怪 */
+}
+.el-card:hover{
+  box-shadow:0px 0px 0px gray;
+}
+.bg1{
+  background-color: greenyellow;
+}
+.bg2{
+  background-color: orange;
+}
+.bg3{
+  background-color: skyblue;
+}
+.bg4{
+  background-color: lemonchiffon;
+}
+.card-icon{
+  font-size:50px;
+  text-align: center;
+  line-height: 100px;
+  width:100px;
+  height:100px;
+}
+:deep(.card-body){
+  display: flex;
+  padding:0;
+}
+.card-content{
+  flex:1;
+  font-size:16px;
+  color:#999;
+  line-height: 100px;
+  text-align: center;
+  padding:0 20px;
 }
 </style>
